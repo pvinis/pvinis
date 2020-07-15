64d105d03f06ff9c4f4cc610b92b162d5c21ef75
Pavlos Vinieratos <pvinis@gmail.com>
2020-07-15 11:59:26 +0200

    lets see

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,4 @@
-# echo "WOP" > /tmp/pvinis-commits-data.json
 cp README.md.tmpl README.md
-npx template-file /tmp/pvinis-commits-data.json README.md.tmpl .
+npx template-file /tmp/pvinis-commits-data.json README.md .
 git add README.md
 git commit -m "fresh bad words"
